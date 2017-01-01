410a793e9c9e26f735de4d61ccdb51531ff168d9
Marina Marques <9.marques.marina@gmail.com>
2017-01-01 23:34:59 +0000

    Copyright date updated through js

--- js/main.js
+++ js/main.js
@@ -103,3 +103,12 @@ jQuery.noConflict();
             });
     });
 })(jQuery);
+
+
+// update footer copyright year
+
+var today = new Date();
+var year = today.getFullYear();
+
+var copyright = document.getElementById("copyright");
+copyright.innerHTML = '© Marina Marques '+ year;
